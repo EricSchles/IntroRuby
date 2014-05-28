31c0ca33e283a292e407a5bd04ba7f1def28e9ee
EricSchles <ericschles@gmail.com>
2014-05-28 07:30:37 -0700

    exercise 2 updated

--- chapter3/exercise2.rb
+++ chapter3/exercise2.rb
@@ -1,10 +1,10 @@
 puts "Enter winter temperature"
-temp_winter = gets
+temp_winter = gets.to_i
 puts "Enter spring temperature"
-temp_spring = gets
+temp_spring = gets.to_i
 puts "Enter summer temperature"
-temp_summer = gets
+temp_summer = gets.to_i
 puts "Enter fall temperature"
-temp_fall = gets
-ave_temp = (temp_winter.to_i + temp_spring.to_i + temp_summer.to_i + temp_fall.to_i)/4
+temp_fall = gets.to_i
+ave_temp = (temp_winter + temp_spring + temp_summer + temp_fall)/4
 puts "The average temperature is #{ave_temp}"
