5ffbdd50e685fb3051d3cadc6568e6e8e75f88d4
EricSchles <ericschles@gmail.com>
2014-05-28 08:38:38 -0700

    exercise 2 chapter 4

---
+++ chapter4/compare2.rb
@@ -0,0 +1,8 @@
+puts "Please input a number in the range 5-10"
+num = gets.to_i
+
+if (( num >=5 ) and (num <=10))
+ puts "Good, you did what I asked."
+else
+ puts "Why didn't you do what I asked?"
+end
