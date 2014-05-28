ec329c7ac543306d51db48de32fcfab987ee8de6
EricSchles <ericschles@gmail.com>
2014-05-28 12:59:07 -0700

    solver for systems linear equations

--- chapter4/ex8.rb
+++ chapter4/ex8.rb
@@ -3,23 +3,25 @@
 
 puts "Input the first equation"
 puts "Input the coefficient on y"
-y_1 = gets.to_f
+y_1 = gets.to_i
 puts "Input the coefficient on x"
-x_1 = gets.to_f
+x_1 = gets.to_i
 puts "Input the y intercept"
-b_1 = gets.to_f
+b_1 = gets.to_i
 
 puts "Input the second equation"
 puts "Input the coefficient on y"
-y_2 = gets.to_f
+y_2 = gets.to_i
 puts "Input the coefficient on x"
-x_2 = gets.to_f
+x_2 = gets.to_i
 puts "Input the y intercept"
-b_2 = gets.to_f
+b_2 = gets.to_i
 
 tmp = 0
 conn_one = false # if we can row reduce based on x's
 conn_two = false
+conn_three = false
+conn_four = false
 solved_condition = (((x_1 == 1 and x_2 == 0 ) and (y_1 == 0 and y_2 == 1)) or ((x_1 == 0 and x_2 == 1) and (y_1 == 1 and y_2 == 0)))
 
 while !solved_condition do
@@ -45,27 +47,69 @@ while !solved_condition do
  end
  # R2 <- R2 - cR1
  if ((x_1 != 0) or ( x_2 != 0))
+ 
  if ( x_1 < x_2 )
-   conn_one = true
-   for i in (-x_2..x_2).step(0.01)
+   for i in (0..x_2)
     if (x_1*i == x_2)
+      conn_one = true
       tmp = i
       break
     end
    end
  end
  if ( x_2 < x_1 )
-   conn_two = true
-   for i in (-x_1..x_1).step(0.01)
+   for i in (0..x_1)
     if ( x_2 * i == x_1)
+      conn_two = true
       tmp = i
       break
     end
    end
  end
- if (!conn_one and !conn_two)
-  if ( y_1
+ elsif (( y_1 != 0) or (y_2 != 0)) 
+  if ( y_1 < y_2 )
+   for i in (0..y_2)
+    if (y_1*i == y_2)
+      conn_three = true
+      tmp = i
+      break
+    end
+   end
+ end
+ if ( y_2 < y_1 )
+   for i in (0..y_1)
+    if ( y_2 * i == y_1)
+      conn_four = true
+      tmp = i
+      break
+    end
+   end
+ end
+ end
 
+ if (conn_one)
+  x_2 = x_2 - x_1 * tmp
+  y_2 = y_2 - y_1 * tmp
+  b_2 = b_2 - b_1 * tmp
+ elsif (conn_two)
+  x_1 = x_1 - x_2 * tmp
+  y_1 = y_1 - y_2 * tmp
+  b_1 = b_1 - b_2 * tmp
+ elsif (conn_three)
+  x_2 = x_2 - x_1 * tmp
+  y_2 = y_2 - y_1 * tmp
+  b_2 = b_2 - b_1 * tmp
+ elsif (conn_four)
+  x_1 = x_1 - x_2 * tmp
+  y_1 = y_1 - y_2 * tmp
+  b_1 = b_1 - b_2 * tmp
+ else
+  puts "There is no solution"
+  break
+ end
+end
 
+puts "#{b_1} = x"
+puts "#{b_2} = y" 
 
 
