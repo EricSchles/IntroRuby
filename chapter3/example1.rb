1113d1e37bd1b5b10cc03399708eeeb92902ed22
EricSchles <ericschles@gmail.com>
2014-05-28 07:13:08 -0700

    first exercise done

---
+++ chapter3/example1.rb
@@ -0,0 +1,8 @@
+puts "what is the length?"
+length_input = gets
+puts "What is the width?"
+width_input = gets
+length = length_input.to_i
+width = width_input.to_i
+area = length * width
+puts "#{area} is the area"
